856f8877d152c479d1ea5c234a24cf2245f696e4
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2023-11-02 10:26:07 -0500

    started profile exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Your Name
+            //Your Favorite Color
+            //Your Favorite Animal
+            //Your Favorite Band
+
+            Console.WriteLine("Hello, what is your name?");
+
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($" Hi! {userName}. What is your favorite color?");
+
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine($"{favoriteColor} is a great color! What is your favorite animal?");
+
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("Good choice! What's your favoite band?");
+
+            var favoriteBand = Console.ReadLine();
+
+            Console.WriteLine("Cool! That's mine too!");
+
         }
     }
 }
