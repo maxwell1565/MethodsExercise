e2b79caf69abd2f0409e934b52d286a4a9b3ef5a
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2024-02-14 15:55:41 -0600

    finished methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,17 +1,59 @@
 ﻿using System.Drawing;
+using System.Reflection;
 
 namespace MethodsExercise
 {
-    public class Program
+    class Program
+
     {
+        //-----------exercise 2-----------
+        public static int Sum(int num1, int num2)
+        {
+        var answer = num1 + num2;
+        return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+        public static int Divide(int num1, int num2)
+        { 
+            return num1 / num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
         static void Main(string[] args)
         {
-            //Your Name
-            //Your Favorite Color
-            //Your Favorite Animal
-            //Your Favorite Band
+            //Declare 5 methods:
+            //1 Add method
+            //1 Subtract method
+            //1 Multiply method
+            //1 Divide method
+            //1 Modulus method
 
-            Console.WriteLine("Hello, what is your name?");
+            var amountOfCars = Sum(2, 6);
+            var amountOfDogs = Multiply(33, 1, 20);
+            var amountOfTrucks = Divide(10, 5);
+            var amountOfCats = Subtract(20, 5);
+            var amountOfLeftovers = Modulus(40, 9);
+
+            Console.WriteLine($"{amountOfCars}, {amountOfDogs}, {amountOfTrucks}, {amountOfCats}, {amountOfLeftovers}");
+
+        //-------------exercise 1-------------
+        //Your Name
+        //Your Favorite Color
+        //Your Favorite Animal
+        //Your Favorite Band
+
+        Console.WriteLine("Hello, what is your name?");
 
             var userName = Console.ReadLine();
 
